9fde4f766e4277234d7d7d1f9f8ecb0a3061140b
Zehir <zehir@zorim.fr>
2022-05-24 00:23:58 +0200

    Add navigation

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,19 +1,58 @@
+<script setup lang="ts">
+const drawer = ref(false)
+const items = ref([
+  { text: 'Home', icon: 'mdi-home', to: '/' },
+  { text: 'Discovery', icon: 'mdi-router-wireless', to: '/gateway/discovery' },
+])
+</script>
+
 <template>
-  <v-navigation-drawer app>
-    <!-- -->
+  <v-navigation-drawer
+    v-model="drawer"
+    bottom
+    temporary
+  >
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :value="item"
+        active-color="primary"
+        :to="item.to"
+      >
+        <v-list-item-avatar>
+          <v-icon :icon="item.icon" />
+        </v-list-item-avatar>
+
+        <v-list-item-title>
+          {{ item.text }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 
-  <v-app-bar app>
+  <v-app-bar app prominent>
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
+
+    <v-toolbar-title>My files</v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn variant="text" icon="mdi-magnify" />
+
+    <v-btn variant="text" icon="mdi-filter" />
+
+    <v-btn variant="text" icon="mdi-dots-vertical" />
     <!-- -->
   </v-app-bar>
 
   <!-- Sizes your content based upon application components -->
   <v-main>
     <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-      <!-- If using vue-router -->
-      <router-view />
-    </v-container>
+    <router-view />
   </v-main>
 
   <v-footer app>
--- src/pages/gateway/discovery.vue
+++ src/pages/gateway/discovery.vue
@@ -9,35 +9,36 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <v-card class="d-flex pa-2" outlined tile>
-    <div>
-      <p>Gateway Discovery</p>
-      <v-btn @click.prevent="Gateways.scanGateways">
-        Scan Gateways
-      </v-btn>
-      <p>{{ logs }}</p>
-    </div>
-  </v-card>
+  <v-container fluid>
+    <v-card class="d-flex pa-2" outlined tile>
+      <div>
+        <p>Gateway Discovery</p>
+        <v-btn @click.prevent="Gateways.scanGateways">
+          Scan Gateways
+        </v-btn>
+        <p>{{ logs }}</p>
+      </div>
+    </v-card>
+    <v-card
+      v-for="(gateway, index) in gateways"
+      :key="index"
+      width="400"
+      loading="true"
+    >
+      <v-card-title>{{ gateway.name }} ({{ gateway.id?.slice(-6) }})</v-card-title>
+      <v-card-subtitle>Path : {{ gateway.path }}</v-card-subtitle>
+      <v-card-actions>
+        <v-btn
+          elevation="2"
+          @click="Gateways.setCurrentGateway(gateway.path)"
+        >
+          {{ t('button.select') }}
+        </v-btn>
 
-  <v-card
-    v-for="(gateway, index) in gateways"
-    :key="index"
-    width="400"
-    loading="true"
-  >
-    <v-card-title>{{ gateway.name }} ({{ gateway.id?.slice(-6) }})</v-card-title>
-    <v-card-subtitle>Path : {{ gateway.path }}</v-card-subtitle>
-    <v-card-actions>
-      <v-btn
-        elevation="2"
-        @click="Gateways.setCurrentGateway(gateway.path)"
-      >
-        {{ t('button.select') }}
-      </v-btn>
-
-      <p>
-        {{ gateways.currentGateway }}
-      </p>
-    </v-card-actions>
-  </v-card>
+        <p>
+          {{ gateways.currentGateway }}
+        </p>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
