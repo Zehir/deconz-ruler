60dcaecfcbb99a27f8b3cc702d4791006a8a602f
Zehir <zehir@zorim.fr>
2022-05-24 00:23:50 +0200

    Add mdi icons

--- src/modules/vuetify.ts
+++ src/modules/vuetify.ts
@@ -1,6 +1,9 @@
 import 'vuetify/styles/main.sass'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import { type UserModule } from '~/types'
 import { isDark } from '~/composables'
 
@@ -9,6 +12,13 @@ export const install: UserModule = ({ app }) => {
   const vuetify = createVuetify({
     theme: {
       defaultTheme: isDark ? 'dark' : 'light',
+    },
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
     },
   })
 
