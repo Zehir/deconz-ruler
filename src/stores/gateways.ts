a2427fd3a328794c2f9e3efb7bed197d421188cb
Zehir <zehir@zorim.fr>
2022-06-21 20:19:52 +0000

    Test HRM update with pinia

--- src/stores/gateways.ts
+++ src/stores/gateways.ts
@@ -2,9 +2,26 @@ import { acceptHMRUpdate, defineStore } from 'pinia'
 import type { GatewayCredentials } from '~/interfaces/deconz'
 
 export const useGatewaysStore = defineStore('gateways', () => {
-  const credentials = ref<GatewayCredentials[]>([])
+  const credentials = reactive<Record<string, GatewayCredentials>>({})
+  const credentials_array = ref<GatewayCredentials[]>([])
 
-  return { credentials }
+  const data_array = ref<any[]>([])
+  // data_array.value[0] = 3
+
+  const data_record = reactive({})
+  // data_record.foo = 35
+
+  const test = computed(() => 14)
+
+  /*
+  watch(credentials, (currentValue, oldValue) => {
+    console.log(Object.keys(currentValue).length)
+    console.log('tu')
+    console.log(Object.keys(oldValue).length)
+  })
+  */
+
+  return { credentials, credentials_array, data_array, data_record, test }
 }, {
   // https://github.com/prazdevs/pinia-plugin-persistedstate
 
@@ -12,7 +29,12 @@ export const useGatewaysStore = defineStore('gateways', () => {
 
   persist: {
     paths: [
+      'data_array',
+      'data_record',
+      /*
       'credentials',
+      'credentials_array',
+      */
     ],
   },
 
@@ -21,3 +43,4 @@ export const useGatewaysStore = defineStore('gateways', () => {
 // https://pinia.vuejs.org/cookbook/hot-module-replacement.html
 if (import.meta.hot)
   import.meta.hot.accept(acceptHMRUpdate(useGatewaysStore, import.meta.hot))
+
