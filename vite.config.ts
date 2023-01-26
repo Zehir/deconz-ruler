9d50646242d8dec34588fe3fb26a7acfcda3ccd0
Zehir <zehir@zorim.fr>
2023-01-26 19:30:30 +0000

    Disabling reactivity transform

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       plugins: {
         vue: Vue({
           include: [/\.vue$/, /\.md$/],
-          reactivityTransform: true,
+          reactivityTransform: false,
         }),
       },
     }),
