776b76dcbc682c76250583fcdecce1a36502acc3
Zehir <zehir@zorim.fr>
2023-08-24 16:55:27 +0000

    code sync

--- src/composables/stateMachines/createGatewayMachine.typegen.ts
+++ src/composables/stateMachines/createGatewayMachine.typegen.ts
@@ -1,32 +1,33 @@
-// This file was automatically generated. Edits will be overwritten
 
-export interface Typegen0 {
-  '@@xstate/typegen': true
-  internalEvents: {
-    'xstate.init': { type: 'xstate.init' }
-  }
-  invokeSrcNameMap: {
+  // This file was automatically generated. Edits will be overwritten
 
-  }
-  missingImplementations: {
-    actions: never
-    delays: never
-    guards: never
-    services: never
-  }
-  eventsCausingActions: {
-
-  }
-  eventsCausingDelays: {
-
-  }
-  eventsCausingGuards: {
-
-  }
-  eventsCausingServices: {
-
-  }
-  matchesStates: 'active' | 'inactive'
-  tags: never
-}
-
+  export interface Typegen0 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "xstate.init": { type: "xstate.init" };
+        };
+        invokeSrcNameMap: {
+          
+        };
+        missingImplementations: {
+          actions: never;
+          delays: never;
+          guards: never;
+          services: never;
+        };
+        eventsCausingActions: {
+          
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          
+        };
+        eventsCausingServices: {
+          
+        };
+        matchesStates: "active" | "inactive";
+        tags: never;
+      }
+  
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import Preview from 'vite-plugin-vue-component-preview'
+// import Preview from 'vite-plugin-vue-component-preview'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
@@ -23,7 +23,7 @@ export default defineConfig({
   },
 
   plugins: [
-    Preview(),
+    // Preview(),
 
     VueMacros({
       plugins: {
